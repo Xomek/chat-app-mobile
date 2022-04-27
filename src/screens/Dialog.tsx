e331d110cc87757f40322487a8d85c240e5be6cb
XomekX <z_v_m_x@mail.ru>
2022-04-27 15:48:36 +0700

    created Home and Dialog pages

---
+++ src/screens/Dialog.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { StyleSheet, View } from "react-native";
+
+const Dialog: FC = () => {
+   return <View></View>;
+};
+
+export default Dialog;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { StyleSheet, View } from "react-native";
+
+const Home: FC = () => {
+   return <View></View>;
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
