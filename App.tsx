ad1afacb873b15c5b992814700507c7cb250974f
XomekX <z_v_m_x@mail.ru>
2022-04-27 15:44:47 +0700

    added redux and additional packages

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { StyleSheet, SafeAreaView, StatusBar, Platform } from "react-native";
 import { Colors } from "./src/styles/variables";
+import { Provider } from "react-redux";
+import store from "./src/redux/store";
 
 export default function App() {
    return (
-      <SafeAreaView style={styles.container}>
-         <ExpoStatusBar style="auto" backgroundColor={Colors.BLUE} />
-      </SafeAreaView>
+      <Provider store={store}>
+         <SafeAreaView style={styles.container}>
+            <ExpoStatusBar style="auto" backgroundColor={Colors.BLUE} />
+         </SafeAreaView>
+      </Provider>
    );
 }
 
